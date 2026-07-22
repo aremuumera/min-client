1ee7dd5830002d3569da37e4099cb6eb5ba8edf2
Aremu <aremu.script@gmail.com>
2026-07-22 11:51:39 +0100

    updated the error message format...

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,14 @@ const nextConfig: NextConfig = {
   //     }
   //   ];
   // },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
